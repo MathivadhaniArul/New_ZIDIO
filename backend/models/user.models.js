d01b53724a8466e74f5b7bd1fe3b99fabfa7d60a
iamdanwi <danwichoudhary@gmail.com>
2025-09-04 11:30:19 +0530

    register api is created

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
     },
-    email:{
-    type:String,
-    required:true,
-    unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    role:{
-        type:String,
-        enum:["user","admin"],
-        required:true,
-        default:"user"
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        required: true,
+        default: "user"
     }
 },
-{timestamps:true});
-const User=mongoose.models.User || mongoose.model("User",userSchema);
+    { timestamps: true });
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
